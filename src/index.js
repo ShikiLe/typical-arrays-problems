611e863bb3344b00f6cdebc57c43ddf5a1ed9a0e
Ковалевич Екатерина Олеговна <7_Kovalevich@minjust.internal>
2021-03-11 12:30:38 +0300

    feat: implement all 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+function sort (arr) {
+  let n = arr.length;
+
+  for (let i = 0; i< n-1; i++) {
+      let min = i;
+  for (let j = i +1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+          min = j;
+      }
+  }  
+  let t = arr[min];
+  arr[min] = arr[i];
+  arr[i] = t;
+  }
+  return arr;
+}
 
 exports.min = function min (array) {
-  return 0;
+  let sortArr = sort(array);
+  return sortArr[0];
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  let sortArr = sort(array);
+  return sortArr[array.length - 1];
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+      result += array[i];
+  }
+  return result / array.length;
 }
